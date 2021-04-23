21ef49ad261a91948c5a39d2566fe5afb975b7a9
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-23 19:58:15 +0530

    Created index.js file in store for Vuex

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
 body {
   font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   line-height: 1.6;
-  background: #e8f7f0;
+  background: #fec674;
 }
 .container {
   max-width: 1100px;
