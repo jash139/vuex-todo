bac493ae40ae2f1298c82fa6fa2ee778e9d10aa5
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-24 21:00:28 +0530

    Created FilterTodos component

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h4>Filter Todos:</h4>
+    <select>
+      <option value="200">200</option>
+      <option value="100">100</option>
+      <option value="50">50</option>
+      <option value="20">20</option>
+      <option value="10">10</option>
+      <option value="5">5</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterTodos",
+};
+</script>
+
+<style scoped>
+select {
+  border: none;
+  margin-top: 20px;
+  padding: 0.5rem;
+}
+</style>
