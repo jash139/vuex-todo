6b85e6bb6a20570a532e525e588576a02966c385
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-24 20:49:49 +0530

    Added deleteTodo action and mutation

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -16,12 +16,17 @@ const actions = {
     async addTodo({ commit }, title) {
         const response = await axios.post("https://jsonplaceholder.typicode.com/todos", { title, completed: false });
         commit("newTodo", response.data);
+    },
+    async deleteTodo({ commit }, id) {
+        await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+        commit("deleteTodo", id);
     }
 };
 
 const mutations = {
     setTodos: (state, todos) => (state.todos = todos),
-    newTodo: (state, todo) => (state.todos = [todo, ...state.todos])
+    newTodo: (state, todo) => (state.todos = [todo, ...state.todos]),
+    deleteTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
 };
 
 export default {
