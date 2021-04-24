420716f4f363f0103c84a7db0746a5778aa343a0
Ayush Acharya <ayushjash1323@gmail.com>
2021-04-24 20:12:22 +0530

    Created add new todo action and mutation

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -12,11 +12,16 @@ const actions = {
     async getTodos({ commit }) {
         const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
         commit("setTodos", response.data);
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post("https://jsonplaceholder.typicode.com/todos", { title, completed: false });
+        commit("newTodo", response.data);
     }
 };
 
 const mutations = {
-    setTodos: (state, todos) => (state.todos = todos)
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => (state.todos = [todo, ...state.todos])
 };
 
 export default {
